e26d7da8b968614517db377316e42425eca6bb72
Veena Rao <veena.500apps@gmail.com>
2023-11-25 20:16:57 +0530

    multer added

--- models/review_requests.js
+++ models/review_requests.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// const multer = require('multer');
-// const path = require('path');
-// const AVATAR_PATH = path.join('/upoads/users/avatars');
+const multer = require('multer');
+const path = require('path');
+const AVTAR_PATH = path.join('/upoads/users/avtars');
 
 const reviewRequestSchema = new mongoose.Schema({
     emp : {
@@ -13,26 +13,12 @@ const reviewRequestSchema = new mongoose.Schema({
         type : String
     },
 
-    avatar : {
+    avtar : {
         type : String
     }
 },
 {timestamps : true}
 );
-
-// let storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, path.join(__dirname , '..', AVATAR_PATH))
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//       cb(null, file.fieldname + '-' + uniqueSuffix)
-//     }
-//   });
-  
-  //static methods
-//   reviewRequestSchema.statics.uploadedAvatar = multer({storage:storage}).single('avatar');
-//   reviewRequestSchema.statics.avatarPath = AVATAR_PATH;
 
 const Registeration = mongoose.model('Registeration', reviewRequestSchema);
 module.exports = Registeration;
